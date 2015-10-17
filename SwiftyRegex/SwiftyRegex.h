5be8c961b1b913b7f4389df8463349f2dd782317
Tyler Long <tyler4long@gmail.com>
2015-10-17 08:50:06 +0800

    Initial Commit

---
+++ SwiftyRegex/SwiftyRegex.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyRegex.h
+//  SwiftyRegex
+//
+//  Created by Tyler Long on 17/10/2015.
+//  Copyright © 2015 Tylingsoft. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SwiftyRegex.
+FOUNDATION_EXPORT double SwiftyRegexVersionNumber;
+
+//! Project version string for SwiftyRegex.
+FOUNDATION_EXPORT const unsigned char SwiftyRegexVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyRegex/PublicHeader.h>
+
+
